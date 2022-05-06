20f32a59dd1e002a8e98a86dd0ddef6566cba6d6
HartMason <hartmason85@gmail.com>
2022-05-05 20:17:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Dashboard } from "./Components/Dashboard";
+import { Signup } from "./Components/Signup";
+import Header from "./Components/Header";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {isLoggedIn ? <Dashboard /> : <Signup />}
     </div>
   );
 }
